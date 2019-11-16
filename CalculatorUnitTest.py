36cbcb2cdafd4a6d4523e18e82a89f4c3148b15f
Alexis Franche <alexis.franche@mail.mcgill.ca>
2019-11-15 22:03:20 -0500

    Added multithreading (3 threads) for testing mutant classes

--- CalculatorUnitTest.py
+++ CalculatorUnitTest.py
@@ -2,30 +2,33 @@ import unittest
 import os.path
 import importlib.util
 
-l = []
-path, dirs, files = next(os.walk("./Mutant_files"))
 
-for file in files:
-    spec = importlib.util.spec_from_file_location(file, path+"/"+file)
-    mutant = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(mutant)
-    l.append([file, mutant.Calculator.operation4(mutant.Calculator),  4])
-
-
-class TestSequense(unittest.TestCase):
+class TestSequence(unittest.TestCase):
     pass
+
+
+def setup(test_list,dir):
+    path, dirs, files = next(os.walk("Mutant_files\\"+dir))
+
+    for file in files:
+        spec = importlib.util.spec_from_file_location(file, path + "/" + file)
+        mutant = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(mutant)
+        test_list.append([file, mutant.Calculator.operation4(mutant.Calculator), 4])
 
 
 def test_generator(a, b):
     def test(self):
-        self.assertEqual(a,b)
+        self.assertEqual(a, b)
+
     return test
 
 
-if __name__ == '__main__':
-
-    for t in l:
+def execute(directory):
+    test_list = []
+    setup(test_list, directory)
+    for t in test_list:
         test_name = 'test_%s' % t[0]
         test = test_generator(t[1], t[2])
-        setattr(TestSequense, test_name, test)
-    unittest.main()
+        setattr(TestSequence, test_name, test)
+    unittest.main(module=__name__)
