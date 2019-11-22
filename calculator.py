6701bdc226d82f6828d303575bfccbe8845c71f9
Ahbelanger <hbelanger.antoine@gmail.com>
2019-11-22 13:21:29 -0500

    changed fcts names

--- calculator.py
+++ calculator.py
@@ -19,10 +19,10 @@ class Calculator():
         return result
 
     def use_calculator(self, num1, num2, num3, num4, num5):
-        returned_num1 = self.operation1(self, num1, num2)
-        returned_num2 = self.operation2(self, returned_num1, num3)
-        returned_num3 = self.operation3(self, returned_num2, num4)
-        result = self.operation4(self, returned_num3, num5)
+        returned_num1 = self.subtract(self, num1, num2)
+        returned_num2 = self.add(self, returned_num1, num3)
+        returned_num3 = self.divide(self, returned_num2, num4)
+        result = self.multiply(self, returned_num3, num5)
 
         return result
 
